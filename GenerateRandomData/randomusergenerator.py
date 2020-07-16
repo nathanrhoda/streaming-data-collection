b2965123ca35d7919902305f43fdf9704c631712
Nathan Rhoda <nathan.rhoda@hotmail.com>
2020-07-16 22:39:37 +0200

    json call to retrieve random data

--- GenerateRandomData/randomusergenerator.py
+++ GenerateRandomData/randomusergenerator.py
@@ -1,3 +1,11 @@
+import requests
+
+def GenerateRandomUserData():    
+    url = 'https://randomuser.me/api/'
+    response = requests.get(url)
+    print(response.json())
+
+
 if __name__ == "__main__":
-    print("MONO")
+    GenerateRandomUserData()
     
