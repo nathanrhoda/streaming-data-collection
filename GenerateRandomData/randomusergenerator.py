77be3b2a6b9461937ca216e974d31ba7c8be3902
Nathan Rhoda <nathan.rhoda@hotmail.com>
2020-07-16 22:57:43 +0200

    Call api return a random user

--- GenerateRandomData/randomusergenerator.py
+++ GenerateRandomData/randomusergenerator.py
@@ -1,11 +1,24 @@
 import requests
+import json
 
 def GenerateRandomUserData():    
     url = 'https://randomuser.me/api/'
     response = requests.get(url)
-    print(response.json())
+    json_data = json.loads(response.text)
+    
+    user =json.dumps( {
+        "First": json_data["results"][0]['name']['first'],
+        "Last": json_data["results"][0]['name']['last'],
+        "Age": json_data["results"][0]['dob']['age'],
+        "Gender": json_data["results"][0]['gender'],
+        "Lattitude": json_data["results"][0]['location']['coordinates']['latitude'],
+        "Longitude": json_data["results"][0]['location']['coordinates']['longitude']
+    })
+    return user    
+    
 
 
 if __name__ == "__main__":
-    GenerateRandomUserData()
+    user = GenerateRandomUserData()
+    print(user)
     
