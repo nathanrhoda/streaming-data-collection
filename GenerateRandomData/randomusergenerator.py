15a2c09d917b1f9f9d900c6cbde9c93a9174291b
Nathan Rhoda <nathan.rhoda@hotmail.com>
2020-07-18 23:31:59 +0200

    Implement streaming to kinesis

--- GenerateRandomData/randomusergenerator.py
+++ GenerateRandomData/randomusergenerator.py
@@ -1,36 +1,49 @@
 import requests
 import json
 import boto3
+import uuid
 import time
+import random
 
 def GenerateRandomUserData():      
-    array = []
-    t = 300
-    while t >= 0:
-        url = 'https://randomuser.me/api/'
-        response = requests.get(url)
-        json_data = json.loads(response.text)
+    url = 'https://randomuser.me/api/'
+    response = requests.get(url)
+    json_data = json.loads(response.text)
             
-        user =json.dumps( {
-            "First": json_data["results"][0]['name']['first'],
-            "Last": json_data["results"][0]['name']['last'],
-            "Age": json_data["results"][0]['dob']['age'],
-            "Gender": json_data["results"][0]['gender'],
-            "Lattitude": json_data["results"][0]['location']['coordinates']['latitude'],
-            "Longitude": json_data["results"][0]['location']['coordinates']['longitude']
-        })    
-        array.append(user)
-        time.sleep(1)
-        t -= 1
-    return array    
+    user = {
+        "First": json_data["results"][0]['name']['first'],
+        "Last": json_data["results"][0]['name']['last'],
+        "Age": json_data["results"][0]['dob']['age'],
+        "Gender": json_data["results"][0]['gender'],
+        "Lattitude": json_data["results"][0]['location']['coordinates']['latitude'],
+        "Longitude": json_data["results"][0]['location']['coordinates']['longitude']
+    }
+    
+    return user
     
 
-def ReadAws(array):    
+def WriteToS3(array):    
     s3 = boto3.resource('s3')
-    body = json.dumps(array).encode('UTF-8')
+    body = str(json.dumps(array).encode('UTF-8'))
     s3.Bucket('nathanrhoda-machinelearning').put_object(Key='userdata.json', Body=body)
 
-if __name__ == "__main__":        
-    userArray = GenerateRandomUserData()    
-    ReadAws(userArray)    
+def StreamToKinesis():
+    client = boto3.client('kinesis', 'us-east-2')
+    partitin_key = str(uuid.uuid4())
+
+    while True:
+        try:
+            data = GenerateRandomUserData()        
+            client.put_record(
+                StreamName='nathanrhoda-stream',
+                Data=json.dumps(data),
+                PartitionKey=partitin_key
+            )
+        except Exception as e:
+            print(e)
+
+        time.sleep(random.uniform(0,1))
+
+if __name__ == "__main__":            
+    StreamToKinesis()    
     print('Done')
